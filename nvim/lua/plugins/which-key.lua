318431d4983fdd4ca6885945052507e34bb59e7c
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-10 14:36:37 +0100

    Update which-key delay + relative path on lualine

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,13 +1,10 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  }
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		delay = 1000,
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
