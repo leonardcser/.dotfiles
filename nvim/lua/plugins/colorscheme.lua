b9d655358ac7b4d4e1a57f72b131628dd9307f2f
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-16 08:26:32 +0100

    Customize nord colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,5 +5,13 @@ return {
 		vim.g.nord_disable_background = true
 		vim.g.nord_italic = false
 		vim.cmd([[colorscheme nord]])
+
+		-- HTML
+		vim.cmd([[highlight @tag gui=bold guifg=#81A1C1]])
+		vim.cmd([[highlight @tag.delimiter gui=bold guifg=#616E88]])
+		vim.cmd([[highlight @tag.attribute guifg=#9876AA]])
+
+		-- Goto labels & function decorators
+		vim.cmd([[highlight @attribute guifg=#EBCB8B]])
 	end,
 }
