913b7d4c997ab7b2ce53ce21faa6a428e2e71a35
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-25 07:25:57 +0100

    Add bigfile plugin

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -70,14 +70,6 @@ return {
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		-- for type, icon in pairs(signs) do
-		-- 	local hl = "DiagnosticSign" .. type
-		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		-- end
-
 		mason_lspconfig.setup_handlers({
 			-- default handler for installed servers
 			function(server_name)
