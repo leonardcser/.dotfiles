6d507307bf3a11f1cdd099acf6f4403dff15908a
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-01-20 19:40:45 +0100

    Update nvim configuration + add mtoc

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", ":tab Git<CR>", { desc = "Open git status" })
-		vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Push git changes" })
+		vim.keymap.set("n", "<leader>gp", ":Git push origin<CR>", { desc = "Push git changes" })
 		vim.keymap.set("n", "<leader>gP", ":Git pull --rebase<CR>", { desc = "Pull git changes" })
 		vim.keymap.set("n", "<leader>ga", ":Git add -u<CR>", { desc = "Add git changes" })
 		vim.keymap.set(
