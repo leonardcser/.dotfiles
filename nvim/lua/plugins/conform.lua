788d96c67d95a7d6d8591aa9d688925d5a29954c
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-01-27 14:17:04 +0100

    chore: remove unused plugins

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -19,7 +19,6 @@ return {
 				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
-				python = { "isort", "black" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
@@ -51,5 +50,23 @@ return {
 				timeout_ms = 1000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
 	end,
 }
