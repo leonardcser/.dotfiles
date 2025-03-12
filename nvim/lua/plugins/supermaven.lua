931d22ec6134ba02b7efbc25d9ccb4121de62970
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-03-12 10:59:48 +0100

    feat: use supermaven

---
+++ nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,15 @@
+return {
+	"supermaven-inc/supermaven-nvim",
+	config = function()
+		require("supermaven-nvim").setup({
+			color = {
+				suggestion_color = "#81A1C1",
+				cterm = 244,
+			},
+		})
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+		keymap.set("n", "<leader>lt", ":SupermavenToggle<CR>", { desc = "Toggle LLM Completions" })
+	end,
+}
