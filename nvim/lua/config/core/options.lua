4eac905b31d65b9a6db8d8b0cebbcc1a6415cbf7
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-04-07 19:51:17 +0200

    feat: set sidescrolloff to 50

--- nvim/lua/config/core/options.lua
+++ nvim/lua/config/core/options.lua
@@ -46,6 +46,7 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.scrolloff = 8
+opt.sidescrolloff = 50
 opt.showmode = false
 opt.ffs = "unix"
 
