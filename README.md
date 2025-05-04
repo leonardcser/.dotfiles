a378e2cf04742b453ad5aa6bab35ce77460df970
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-05-04 16:12:08 +0200

    refactor: readme and post.sh

--- README.md
+++ README.md
@@ -15,18 +15,3 @@ To update the `Brewfile`:
 ```bash
 brew bundle dump
 ```
-
-## Additional dependencies
-
-```bash
-npm install -g @fsouza/prettierd
-```
-
-## MacOS
-
-```bash
-# "Disable" dock
-defaults write com.apple.dock autohide-delay -float 1000; killall Dock
-# Allow to drag windows from anywhere by holding ctrl+cmd (not working)
-defaults write -g NSWindowShouldDragOnGesture -bool true
-```
--- post.sh
+++ post.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# "Disable" dock
+defaults write com.apple.dock autohide-delay -float 1000; killall Dock
+# Allow to drag windows from anywhere by holding ctrl+cmd (not working)
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
+# NPM
+npm install -g @fsouza/prettierd
+
+# Batt
 sudo brew services start batt
 sudo batt limit 80
 sudo batt disable-charging-pre-sleep enable
