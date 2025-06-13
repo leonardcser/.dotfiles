a5704be71d7a2251de84c70727e01f89e767014c
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-06-13 13:22:46 +0200

    feat: update nvim config

---
+++ .config/nvim/lua/plugins/cursortab.lua
@@ -0,0 +1,10 @@
+return {
+	-- "leonardcser/cursortab.nvim",
+	dir = "~/dev/lua/cursortab.nvim",
+	build = "cd server && go build -o cursortab .",
+	config = function()
+		require("cursortab").setup({
+			debug_immediate_shutdown = true,
+		})
+	end,
+}
