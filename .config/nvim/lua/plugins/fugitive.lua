709f41a99a11c59efe40d91fd1684c97c2780cc6
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-08-21 09:23:47 +0200

    feat: update nvim

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gs", ":tab Git<CR>", { desc = "Open git status" })
 		vim.keymap.set("n", "<leader>gp", ":Git push origin<CR>", { desc = "Push git changes" })
-		vim.keymap.set("n", "<leader>gP", ":Git pull --rebase<CR>", { desc = "Pull git changes" })
+		vim.keymap.set("n", "<leader>gP", ":Git pull --rebase --prune<CR>", { desc = "Pull git changes" })
 		vim.keymap.set("n", "<leader>ga", ":Git add -u<CR>", { desc = "Add git changes" })
 		vim.keymap.set(
 			"n",
