8eabf3de55fe8b3adaca2f8d5e69bf84294c1ee8
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-09-15 22:51:22 +0200

    feat(nvim): fix lua lspconfig

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -106,11 +106,29 @@ return {
 					capabilities = capabilities,
 					settings = {
 						Lua = {
+							runtime = {
+								-- Tell the language server which version of Lua you're using
+								version = "LuaJIT",
+							},
 							diagnostics = {
+								-- Get the language server to recognize the `vim` global
 								globals = { "vim" },
+							},
+							workspace = {
+								-- Make the server aware of Neovim runtime files
+								library = {
+									vim.env.VIMRUNTIME,
+									"${3rd}/luv/library",
+									"${3rd}/busted/library",
+								},
+								checkThirdParty = false,
 							},
 							completion = {
 								callSnippet = "Replace",
+							},
+							-- Do not send telemetry data
+							telemetry = {
+								enable = false,
 							},
 						},
 					},
