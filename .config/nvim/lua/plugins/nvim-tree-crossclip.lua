5765495a44cccf15bc7be202d335fe34411f4877
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-09-19 20:37:05 +0200

    feat(nvim): update custom plugins

--- .config/nvim/lua/plugins/nvim-tree-crossclip.lua
+++ .config/nvim/lua/plugins/nvim-tree-crossclip.lua
@@ -2,4 +2,7 @@ return {
 	dir = vim.fn.stdpath("config") .. "/lua/plugins/custom/nvim-tree-crossclip.nvim",
 	name = "nvim-tree-crossclip",
 	dependencies = "nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree-crossclip").setup()
+	end,
 }
