6f324205c9e074afbd0eb6e9d060dbbce923b153
leonardcser <73912641+leonardcser@users.noreply.github.com>
2026-02-10 10:26:08 +0100

    feat: update config

--- .config/nvim/lua/plugins/cursortab.lua
+++ .config/nvim/lua/plugins/cursortab.lua
@@ -11,16 +11,17 @@ return {
 			},
 			provider = {
 				-- url = "http://localhost:8000",
-				type = "sweep",
+				type = "sweepapi",
 				url = "https://llm.box.home.arpa",
 				model = "sweep-next-edit-1.5B-Q8_0",
-				api_key_env = "BOX_AUTH_TOKEN",
+				-- api_key_env = "BOX_AUTH_TOKEN",
+				api_key_env = "SWEEPAI_API_KEY",
 				max_tokens = 1024,
 			},
 			behavior = {
 				max_visible_lines = 4,
 				-- idle_completion_delay = -1, -- Delay in ms after idle to trigger completion (-1 to disable)
-				-- text_change_debounce = -1, -- Debounce in ms after text change to trigger completion (-1 to disable)
+				text_change_debounce = -1, -- Debounce in ms after text change to trigger completion (-1 to disable)
 			},
 			-- blink = {
 			-- 	enabled = true,
