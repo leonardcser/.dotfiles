23a55024247a1a6a9205e43a1377179afdc40d45
leonardcser <73912641+leonardcser@users.noreply.github.com>
2026-04-12 22:49:29 +0200

    feat: update config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,73 +1,45 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
+	branch = "main",
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = false,
-			},
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"java",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c",
-				"cpp",
-			},
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
+		require("nvim-treesitter").setup({})
+
+		require("nvim-treesitter").install({
+			"json",
+			"java",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"cpp",
 		})
 
-		-- import nvim-treesitter plugin
 		require("nvim-ts-autotag").setup({
 			opts = {
-				-- Defaults
-				enable_close = true, -- Auto close tags
-				enable_rename = true, -- Auto rename pairs of tags
-				enable_close_on_slash = false, -- Auto close on trailing </
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = false,
 			},
-			-- Also override individual filetype configs, these take priority.
-			-- Empty by default, useful if one of the "opts" global settings
-			-- doesn't work well in a specific filetype
 			per_filetype = {
 				["html"] = {
 					enable_close = false,
