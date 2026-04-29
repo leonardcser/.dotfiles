01233fee96e914b15c9782de270861efbee62479
leonardcser <73912641+leonardcser@users.noreply.github.com>
2026-04-29 23:46:22 +0200

    feat: update config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,39 +7,23 @@ return {
 		"windwp/nvim-ts-autotag",
 	},
 	config = function()
-		require("nvim-treesitter").setup({})
-
-		require("nvim-treesitter").install({
-			"json",
-			"java",
-			"javascript",
-			"typescript",
-			"tsx",
-			"yaml",
-			"html",
-			"css",
-			"prisma",
-			"markdown",
-			"markdown_inline",
-			"svelte",
-			"graphql",
-			"bash",
-			"lua",
-			"vim",
-			"dockerfile",
-			"gitcommit",
-			"git_rebase",
-			"diff",
-			"gitignore",
-			"query",
-			"vimdoc",
-			"c",
-			"cpp",
-		})
+		local nts = require("nvim-treesitter")
+		nts.setup({})
 
 		vim.api.nvim_create_autocmd("FileType", {
+			desc = "Start treesitter and install parser if missing",
 			callback = function(args)
-				pcall(vim.treesitter.start, args.buf)
+				local lang = vim.treesitter.language.get_lang(args.match)
+				if not lang then
+					return
+				end
+				if not vim.list_contains(nts.get_available(), lang) then
+					return
+				end
+				if not vim.list_contains(nts.get_installed(), lang) then
+					nts.install(lang):wait(300000)
+				end
+				vim.treesitter.start(args.buf, lang)
 			end,
 		})
 
